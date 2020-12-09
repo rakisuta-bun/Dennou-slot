a556a20e672990603ab6fa3b90f52638c2ab946f
rakisuta_bun <raki0142@gmail.com>
2020-12-09 11:31:57 +0900

    シャッフルできたぞ！！！！

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,36 @@
+let array = [];
+
+array = [
+  // '<img src="images/bar.png" class="img-fluid" alt="Responsive image">',
+  // '<img src="images/cherry.png" class="img-fluid" alt="Responsive image">',
+  // '<img src="images/prum.png" class="img-fluid" alt="Responsive image">',
+  // '<img src="images/seven.png" class="img-fluid" alt="Responsive image">',
+  // '<img src="images/suika.png" class="img-fluid" alt="Responsive image">',
+  // '<img src="images/suneo.png" class="img-fluid" alt="Responsive image">',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+];
+// ・文字をランダムで出力するプログラムをつくろう
+// 1. HTMLファイルに、ランダム文字列表示用の要素をつくる（  <span id="rand1"></span> ）
+// 2. 関数 shuffle(id)関数を作成する。引数は、上記要素のidを渡すためのid文字列を持たせる
+// 3. 関数 shuffle(id)では、一定時間ごとに処理を呼び出すsetInterval()を使って、文字列を定期的に変更する
+
+function shuffle(id) {
+  setInterval(function() {
+    const roll = [Math.floor(Math.random() * array.length)];
+    console.log(array[roll])
+    document.getElementById("rand1").innerHTML = (array[roll]);
+  }), 100
+};
+
+
 function start(){
   start_lever()
   start_stop_buttons()
+  shuffle()
 }
 function start_lever(){
   const obj = document.getElementById("start-button")
