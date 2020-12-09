ae1be333d25f4c4b0425522e69bf982a687679d3
rakisuta_bun <raki0142@gmail.com>
2020-12-09 16:24:27 +0900

    スタートボタン正規化ストップボタンと絵柄の実装

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,52 @@
-let array = [];
-array = [
-  // '<img src="images/bar.png" class="img-fluid" alt="Responsive image">',
-  // '<img src="images/cherry.png" class="img-fluid" alt="Responsive image">',
-  // '<img src="images/prum.png" class="img-fluid" alt="Responsive image">',
-  // '<img src="images/seven.png" class="img-fluid" alt="Responsive image">',
-  // '<img src="images/suika.png" class="img-fluid" alt="Responsive image">',
-  // '<img src="images/suneo.png" class="img-fluid" alt="Responsive image">',
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
+let rolls = [];
+rolls = [
+  '<img src="images/bar.png" class="img-fluid" style="height: 100px">',
+  '<img src="images/cherry.png" class="img-fluid" style="height: 100px">',
+  '<img src="images/prum.png" class="img-fluid" style="height: 100px">',
+  '<img src="images/seven.png" class="img-fluid" style="height: 100px">',
+  '<img src="images/suika.png" class="img-fluid" style="height: 100px">',
+  '<img src="images/suneo.png" class="img-fluid" style="height: 100px">',
 ];
-let intervalID = 0;
+let intervalTimers = [
+    null, null, null
+];
 // ・文字をランダムで出力するプログラムをつくろう
 // 1. HTMLファイルに、ランダム文字列表示用の要素をつくる（  <span id="rand1"></span> ）
 // 2. 関数 shuffle(id)関数を作成する。引数は、上記要素のidを渡すためのid文字列を持たせる
 // 3. 関数 shuffle(id)では、一定時間ごとに処理を呼び出すsetInterval()を使って、文字列を定期的に変更する
 
 function shuffle(id) {
-  intervalID = setInterval(function() {
-    const roll = [Math.floor(Math.random() * array.length)];
-    console.log(array[roll])
-    document.getElementById("rand1").innerHTML = (array[roll]);
+  intervalTimers[id] = setInterval(function() {
+    const roll = [Math.floor(Math.random() * rolls.length)];
+    console.log(rolls[roll])
+    const elementId = "rand" + id // rand0 ~ rand2
+    document.getElementById(elementId).innerHTML = rolls[roll];
   }), 100
-};
+}
 
+function stop(id) {
+  clearTimeout(intervalTimers[id]);
+  intervalTimers[id] = null;
+}
 
-function start(){
+function isStopAllSlots() {
+  let result0 = intervalTimers[0] === null; // intervalTimersのひとつめの要素がヌルであること(true/false)
+  let result1 = intervalTimers[1] === null;
+  let result2 = intervalTimers[2] === null;
+
+  return result0 && result1 && result2;
+}
+
+function start() {
+  if (isStopAllSlots() === false) {
+    return false
+  }
+
   start_lever()
   start_stop_buttons()
-  shuffle()
-}
-function stop(){
-
+  shuffle(0)
+  shuffle(1)
+  shuffle(2)
 }
 function start_lever(){
   const obj = document.getElementById("start-button")
@@ -51,15 +64,16 @@ function start_stop_buttons(){
   obj3.classList.add("btn-primary");
   obj3.classList.remove("btn-secondary");
 }
-document.getElementById("start-button").onclick = start
-;
+document.getElementById("start-button").onclick = start;
 document.getElementById("stop-1").onclick = function() {
   this.classList.toggle("btn-danger");
-  clearTimeout(intervalID)
-};
+  stop(0);
+}
 document.getElementById("stop-2").onclick = function() {
   this.classList.toggle("btn-danger");
-};
+  stop(1);
+}
 document.getElementById("stop-3").onclick = function() {
   this.classList.toggle("btn-danger");
-};
+  stop(2);
+}
